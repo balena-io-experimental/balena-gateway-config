3c03e64e77f566e2d7b12b836e80d4ed1ea6fa7f
Joseph Kogut <joseph@balena.io>
2021-06-23 15:23:08 -0700

    move function wait_for_dbus() to standalone script
    Signed-off-by: Joseph Kogut <joseph@balena.io>

--- bluez/entry.sh
+++ bluez/entry.sh
@@ -1,22 +1,6 @@
 #!/bin/sh
 
-function wait_for_dbus() {
-	while true; do
-		dbus-send --system \
-			  --print-reply \
-			  --dest=org.freedesktop.DBus \
-			  /org/freedesktop/DBus \
-			  org.freedesktop.DBus.ListNames
-
-		if [ $? ]; then
-			break;
-		else
-			sleep 0.1
-		fi
-	done
-
-	echo "DBus is now accepting connections"
-}
+source dbus-wait.sh
 
 rfkill block bluetooth && rfkill unblock bluetooth
 wait_for_dbus \
--- connman/entry.sh
+++ connman/entry.sh
@@ -1,23 +1,6 @@
 #!/bin/sh
 
-function wait_for_dbus() {
-	while true; do
-		dbus-send --system \
-			  --print-reply \
-			  --dest=org.freedesktop.DBus \
-			  /org/freedesktop/DBus \
-			  org.freedesktop.DBus.ListNames
-
-		if [ $? ]; then
-			break;
-		else
-			sleep 0.1
-		fi
-	done
-
-	echo "DBus is now accepting connections"
-}
-
+source dbus-wait.sh
 
 wait_for_dbus \
 	&& wpa_supplicant -u -B \
