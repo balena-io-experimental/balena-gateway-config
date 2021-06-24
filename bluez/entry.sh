24b01114f53b5d0a7048c31a20dac1c9321c9cc4
Joseph Kogut <joseph@balena.io>
2021-06-24 12:16:33 -0700

    remove iface-setup container
    Move setup scripts to other containers that require those actions.
    Prepend dbus-send commands issued to host OS with appropriate
    DBUS_SYSTEM_BUS_ADDRESS inside those containers.
    
    Signed-off-by: Joseph Kogut <joseph@balena.io>

--- bluez/entry.sh
+++ bluez/entry.sh
@@ -2,6 +2,9 @@
 
 source dbus-wait.sh
 
+DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket \
+	./systemd-stop-unit.sh bluetooth.service
+
 rfkill block bluetooth && rfkill unblock bluetooth
 wait_for_dbus \
 	&& /usr/lib/bluetooth/bluetoothd  --nodetach
