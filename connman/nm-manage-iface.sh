fab109a498b89d19b4cd28318a6350c4b07be874
Joseph Kogut <joseph@balena.io>
2024-04-04 07:04:27 -0700

    nm-manage-iface: fix error handling
    Signed-off-by: Joseph Kogut <joseph@balena.io>

--- connman/nm-manage-iface.sh
+++ connman/nm-manage-iface.sh
@@ -14,23 +14,21 @@ device_path=$(dbus-send --system \
 			org.freedesktop.NetworkManager.GetDeviceByIpIface \
 			string:"$iface" | awk '/object path/ {print $3}')
 
-if [ $? != 0 ] || [ -z "$device_path" ]; then
+if [ -z "$device_path" ]; then
 	echo "failed getting device object path"
-	ret 1
+	exit 1
 fi
 
-dbus-send --system \
-	  --print-reply \
-	  --dest=org.freedesktop.NetworkManager \
-	  $(echo $device_path | xargs) \
-	  org.freedesktop.DBus.Properties.Set \
-	  string:"org.freedesktop.NetworkManager.Device" \
-	  string:"Managed" \
-	  variant:boolean:$managed 1>&2 > /dev/null
 
-state=$([ $managed = "true" ] && echo "managed" || echo "unmanaged")
-
-if [ $? = 0 ]; then
+if dbus-send --system \
+	     --print-reply \
+	     --dest=org.freedesktop.NetworkManager \
+	     "$(echo "$device_path" | xargs)" \
+	     org.freedesktop.DBus.Properties.Set \
+	     string:"org.freedesktop.NetworkManager.Device" \
+	     string:"Managed" \
+	     variant:boolean:"$managed" 1>&2 > /dev/null; then
+	state=$([ "$managed" = "true" ] && echo "managed" || echo "unmanaged")
 	echo "$iface is now $state"
 else
 	echo "failed making $iface $state: $?"
